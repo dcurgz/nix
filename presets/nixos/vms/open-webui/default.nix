3c1c5fa80cd8434288d3bdf738f518ca5f3c510f
Dylan Curzon <hyperberry@curz.sh>
2026-02-13 16:28:11 +0000

    open-webui: configure gpu on boot

--- presets/nixos/vms/open-webui/default.nix
+++ presets/nixos/vms/open-webui/default.nix
@@ -48,11 +48,9 @@ in
         ipAddress = "10.0.0.19";
       };
 
-      # Allow unfree packages for this VM
       nixpkgsConfig = {
         config.allowUnfree = true;
         config.nvidia.acceptLicense = true;
-        config.cudaSupport = true;
       };
 
       mounts = [
@@ -91,158 +89,168 @@ in
       ];
 
       # VM-specific configuration
-      config = {
-        imports = [
-          "${NIXOS_PRESETS}/packages/core"
-          # define media, data groups
-          "${NIXOS_PRESETS}/security/groups"
-        ];
+      config =
+        let
+          nvidia = config.boot.kernelPackages.nvidiaPackages.stable;
+        in
+        {
+          imports = [
+            "${NIXOS_PRESETS}/packages/core"
+            # define media, data groups
+            "${NIXOS_PRESETS}/security/groups"
+          ];
 
-        nix.channel.enable = false;
+          nix.channel.enable = false;
 
-        services.open-webui = {
-          enable = true;
-          host = "0.0.0.0";
-          port = internal_port;
-        };  
-
-        systemd.services.open-webui.serviceConfig = {
-          DynamicUser = lib.mkForce false;
-          User = "open-webui";
-        };
-
-        users.users.open-webui = {
-          isSystemUser = true;
-          group = "open-webui";
-          extraGroups = [ "data" ];
-        };
-        users.groups.open-webui = {};
-
-        services.nginx = 
-          let
-            address = "vm-openwebui.${secrets.tailscale.magic_dns}";
-          in
-          {
+          services.open-webui = {
             enable = true;
-            recommendedGzipSettings = true;
-            recommendedOptimisation = true;
-            recommendedProxySettings = true;
-            recommendedTlsSettings = true;
-            virtualHosts."${address}" = {
-              default = true;
-              forceSSL = true;
-              sslCertificate = "/etc/ssl/certs/${address}.crt";
-              sslCertificateKey = "/etc/ssl/certs/${address}.key";
-              locations."/" = {
-                proxyPass = "http://localhost:${toString internal_port}";
-                proxyWebsockets = true;
+            host = "0.0.0.0";
+            port = internal_port;
+          };  
 
-                extraConfig = ''
-                  client_max_body_size 16G;
-                '';
+          systemd.services.open-webui.serviceConfig = {
+            DynamicUser = lib.mkForce false;
+            User = "open-webui";
+          };
+
+          users.users.open-webui = {
+            isSystemUser = true;
+            group = "open-webui";
+            extraGroups = [ "data" ];
+          };
+          users.groups.open-webui = {};
+
+          services.nginx = 
+            let
+              address = "vm-openwebui.${secrets.tailscale.magic_dns}";
+            in
+            {
+              enable = true;
+              recommendedGzipSettings = true;
+              recommendedOptimisation = true;
+              recommendedProxySettings = true;
+              recommendedTlsSettings = true;
+              virtualHosts."${address}" = {
+                default = true;
+                forceSSL = true;
+                sslCertificate = "/etc/ssl/certs/${address}.crt";
+                sslCertificateKey = "/etc/ssl/certs/${address}.key";
+                locations."/" = {
+                  proxyPass = "http://localhost:${toString internal_port}";
+                  proxyWebsockets = true;
+
+                  extraConfig = ''
+                    client_max_body_size 16G;
+                  '';
+                };
               };
+            };
+          # give access to certs
+          users.users.nginx.extraGroups = [ "data" ];
+
+          boot = {
+            kernelModules = [
+              "nvidia"
+              "nvidia_modeset"
+              "nvidia_drm"
+              "nvidia_uvm"
+            ];
+
+            kernelParams = [
+              "nvidia-drm.modeset=1"
+              "nvidia-drm.fbdev=1"
+            ];
+          };
+
+          services.xserver.videoDrivers = [ "nvidia" ];
+
+          hardware.graphics = {
+            enable = true;
+            extraPackages = with pkgs; [
+              #libva
+              #libva-utils
+              #libva-vdpau-driver
+              #libvdpau
+              #libvdpau-va-gl
+              #nvidia-vaapi-driver
+              #vdpauinfo
+            ];
+          };
+
+          hardware.nvidia = {
+            #forceFullCompositionPipeline = true;
+            modesetting.enable = true;
+            powerManagement.enable = true;
+            open = true;
+            nvidiaSettings = false;
+            nvidiaPersistenced = true;
+            package = nvidia;
+          };
+
+          services = {
+            ollama = {
+              enable = true;
+              user = "ollama";
+              group = "ollama";
+              #openFirewall = true;
+
+              package = pkgs.ollama-cuda;
+
+              host = "0.0.0.0";
+              port = 11434;
+
+              #home = "/var/lib/ollama";
+
+              #environmentVariables = {
+              #  OLLAMA_FLASH_ATTENTION = "true";
+              #  OLLAMA_CONTEXT_LENGTH = "32768";
+  # OLLAMA_CON#TEXT_LENGTH = "16384";
+              #  OLLAMA_KV_CACHE_TYPE = "q8_0";
+              #  OLLAMA_KEEP_ALIVE = "10m";
+              #  OLLAMA_MAX_LOADED_MODELS = "4";
+              #  OLLAMA_MAX_QUEUE = "64";
+              #  OLLAMA_NUM_PARALLEL = "1";
+              #  OLLAMA_ORIGINS = "*";
+              #};
             };
           };
 
-        boot = {
-          kernelModules = [
-            "nvidia"
-            "nvidia_modeset"
-            "nvidia_drm"
-            "nvidia_uvm"
-          ];
+         # systemd.services.ollama.serviceConfig = {
+         #   DeviceAllow = lib.mkForce [ ];
+         #   DevicePolicy = lib.mkForce "auto";
+         # };
 
-          kernelParams = [
-            "nvidia-drm.modeset=1"
-            "nvidia-drm.fbdev=1"
-          ];
-        };
+         # users.users.ollama = {
+         #   isSystemUser = true;
+         #   group = "ollama";
+         # };
+         # users.groups.ollama = {};
 
-        services.xserver.videoDrivers = [ "nvidia" ];
-
-        hardware.graphics = {
-          enable = true;
-          extraPackages = with pkgs; [
-            #libva
-            #libva-utils
-            #libva-vdpau-driver
-            #libvdpau
-            #libvdpau-va-gl
-            #nvidia-vaapi-driver
-            #vdpauinfo
-          ];
-        };
-
-        hardware.nvidia = {
-          #forceFullCompositionPipeline = true;
-          modesetting.enable = true;
-          powerManagement.enable = true;
-          open = true;
-          nvidiaSettings = false;
-          nvidiaPersistenced = true;
-          #package = config.boot.kernelPackages.nvidiaPackages.stable;
-        };
-
-        environment.systemPackages = with pkgs; [
-          pciutils
-          lm_sensors
-          btop-cuda
-        ];
-
-        services = {
-          ollama = {
-            enable = true;
-            user = "ollama";
-            group = "ollama";
-            #openFirewall = true;
-
-            package = pkgs.ollama-cuda;
-
-            host = "0.0.0.0";
-            port = 11434;
-
-            #home = "/var/lib/ollama";
-
-            #environmentVariables = {
-            #  OLLAMA_FLASH_ATTENTION = "true";
-            #  OLLAMA_CONTEXT_LENGTH = "32768";
-# OLLAMA_CON#TEXT_LENGTH = "16384";
-            #  OLLAMA_KV_CACHE_TYPE = "q8_0";
-            #  OLLAMA_KEEP_ALIVE = "10m";
-            #  OLLAMA_MAX_LOADED_MODELS = "4";
-            #  OLLAMA_MAX_QUEUE = "64";
-            #  OLLAMA_NUM_PARALLEL = "1";
-            #  OLLAMA_ORIGINS = "*";
-            #};
+          systemd.services.nvidia-gpu-config = {
+            description = "Configure NVIDIA GPU";
+            wantedBy = [ "multi-user.target" ];
+            path = [ nvidia.bin ];
+            # just running `nvidia-smi` seems to be enough to initialize the GPU,
+            # before this must be done before ollama starts.
+            script = "nvidia-smi";
+            serviceConfig.Type = "oneshot";
           };
-        };
 
-       # systemd.services.ollama.serviceConfig = {
-       #   DeviceAllow = lib.mkForce [ ];
-       #   DevicePolicy = lib.mkForce "auto";
-       # };
+          systemd.services.ollama.after = [ "nvidia-gpu-config.service" ];
 
-       # users.users.ollama = {
-       #   isSystemUser = true;
-       #   group = "ollama";
-       # };
-       # users.groups.ollama = {};
+          networking.firewall.allowedTCPPorts = [
+            22
+            80
+            443
+          ];
 
-        users.users.nginx.extraGroups = [ "data" ];
+          services.tailscale.authKeyFile = "/run/credentials/tailscaled.service/tailscale-auth-key";
 
-        networking.firewall.allowedTCPPorts = [
-          22
-          80
-          443
-        ];
-
-        services.tailscale.authKeyFile = "/run/credentials/tailscaled.service/tailscale-auth-key";
-
-        systemd.services.tailscaled = {
-          serviceConfig = {
-            LoadCredential = [ "tailscale-auth-key" ];
+          systemd.services.tailscaled = {
+            serviceConfig = {
+              LoadCredential = [ "tailscale-auth-key" ];
+            };
           };
-        };
       };
     };
   };
