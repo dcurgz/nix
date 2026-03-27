948ace39254a7ab6cd93b7acc0a3a1d8e1ffdbe5
Dylan Curzon <hyperberry@curz.sh>
2026-03-27 11:08:26 +0000

    mc-wg-0: fixup

--- presets/nixos/vms/minecraft-wg-0/default.nix
+++ presets/nixos/vms/minecraft-wg-0/default.nix
@@ -20,79 +20,70 @@ in
 {
   hyperberry.virtualization = {
     vms.${hostname} = {
-      vcpus = 12;
-      mem = 1024 * 16;
-
       networking = {
         macAddress = "02:00:00:00:00:01";
         ipAddress = "10.0.0.11";
-        #forwardPorts = [
-        #  {
-        #    source = 25565;
-        #    target = 25565;
-        #    proto = "udp";
-        #  }
-        #];
       };
+      microvm = {
+        # VM-specific configuration
+        config = {
+          imports = [
+            inputs.nix-minecraft.nixosModules.minecraft-servers
+            "${NIXOS_PRESETS}/packages/core"
+            "${NIXOS_PRESETS}/security/groups"
+          ];
 
-      # Additional shares beyond the common ones
-      mounts = [
-        {
-          source = dataDir;
-          mountPoint = dataDir;
-          tag = "minecraft-data";
-          proto = "virtiofs";
-        }
-      ];
+          microvm.vcpu = 12;
+          microvm.mem = 1024 * 16;
+          microvm.shares = [
+            {
+              source = dataDir;
+              mountPoint = dataDir;
+              tag = "minecraft-data";
+              proto = "virtiofs";
+            }
+          ];
 
-      # VM-specific configuration
-      config = {
-        imports = [
-          inputs.nix-minecraft.nixosModules.minecraft-servers
-          "${NIXOS_PRESETS}/packages/core"
-          "${NIXOS_PRESETS}/security/groups"
-        ];
+          networking.firewall.allowedTCPPorts = [ 25565 ];
+          networking.firewall.allowedUDPPorts = [ 25565 ];
 
-        networking.firewall.allowedTCPPorts = [ 25565 ];
-        networking.firewall.allowedUDPPorts = [ 25565 ];
+          environment.systemPackages = with pkgs; [
+            javaPackages.compiler.temurin-bin.jre-17
+            rcon-cli
+          ];
 
-        # TODO: Declaratively define neoforge server
-        environment.systemPackages = with pkgs; [
-          #jre17_minimal
-          javaPackages.compiler.temurin-bin.jre-17
-          rcon-cli
-        ];
-
-        users.users.minecraft = {
-          isSystemUser = true;
-          group = "minecraft";
-          extraGroups = [ "data" ];
-        };
-        users.groups.minecraft = {};
-
-        systemd.services.minecraft =
-          let
-            serverPath = "${dataDir}/wg-forge-1.20.1/";
-          in
-          {
-            enable = true;
-            after = [ "network-online.target" ];
-            wants = [ "network-online.target" ];
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              Restart = "always";
-              RestartSec = "5s";
-              WorkingDirectory = serverPath;
-              User = "minecraft";
-            };
-            path = [ pkgs.bash pkgs.javaPackages.compiler.temurin-bin.jre-17 ];
-            script = ''
-              #!/usr/bin/env bash
-              set -xe
-              bash ./run.sh
-            '';
+          users.users.minecraft = {
+            isSystemUser = true;
+            group = "minecraft";
+            extraGroups = [ "data" ];
           };
+          users.groups.minecraft = {};
+
+          systemd.services.minecraft =
+            let
+              serverPath = "${dataDir}/wg-forge-1.20.1/";
+            in
+            {
+              enable = true;
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                Restart = "always";
+                RestartSec = "5s";
+                WorkingDirectory = serverPath;
+                User = "minecraft";
+              };
+              path = [ pkgs.bash pkgs.javaPackages.compiler.temurin-bin.jre-17 ];
+              script = ''
+                #!/usr/bin/env bash
+                set -xe
+                bash ./run.sh
+              '';
+            };
+        };
       };
+
     };
   };
 }
