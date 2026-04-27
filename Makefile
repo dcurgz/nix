7810c832055af3ecfda52bb3e5ad4b6e29746d49
Dylan Curzon <hyperberry@curz.sh>
2026-04-27 17:03:20 +0100

    dcurgz.me: update index page, add code-formatting with chroma

--- Makefile
+++ Makefile
@@ -13,6 +13,9 @@ CHECK_MINECRAFT := ssh vm-mc-leedlemon "rcon-cli --password leedlemon list" || t
 
 .PHONY: hyperberry piberry airberry update
 
+update-index:
+	git add -A
+
 # weird Nix bug where local relative flake inputs are broken when they are
 # garbage collected from the nix store
 update-local-inputs:
@@ -21,7 +24,7 @@ update-local-inputs:
 	nix flake update weirdfish-server
 
 # Build host-specific configurations
-hyperberry:
+hyperberry: update-index
 	# Fix permissions
 	sudo chown -R root:wheel /etc/nixos
 	sudo chmod -R 774 /etc/nixos
@@ -32,38 +35,38 @@ hyperberry:
 		sudo nixos-rebuild switch --flake .#hyperberry ; \
 	fi
 
-blueberry:
+blueberry: update-index
 	# Fix permissions
 	sudo chown -R root:wheel /etc/nixos
 	sudo chmod -R 774 /etc/nixos
 	# Go!
 	sudo nixos-rebuild --no-reexec switch --flake .#blueberry $(NOM)
 
-piberry:
+piberry: update-index
 	sudo nixos-rebuild $(REMOTE_BUILDER) --max-jobs 0 switch --flake .#piberry
 
-piberry-sdcard:
+piberry-sdcard: update-index
 	sudo $(NIX) build .#nixosConfigurations.piberry.config.system.build.images.sd-card
 
-tauberry:
+tauberry: update-index
 	sudo nixos-rebuild $(REMOTE_BUILDER) --max-jobs 0 switch --flake .#tauberry
 
-tauberry-sdcard:
+tauberry-sdcard: update-index
 	sudo $(NIX) build .#nixosConfigurations.tauberry.config.system.build.images.sd-card
 
-airberry:
+airberry: update-index
 	sudo darwin-rebuild switch --option builders "ssh://builder@miniberry aarch64-darwin - 16 1" --max-jobs 0 --flake .#airberry 
 
-miniberry:
+miniberry: update-index
 	sudo darwin-rebuild switch --flake .#miniberry --show-trace
 
-bootstrap-weirdfi.sh:
+bootstrap-weirdfi.sh: update-index
 	$(NIX) run github:nix-community/nixos-anywhere -- --generate-hardware-config nixos-generate-config ./systems/weirdfi.sh-cax11-4gb/hardware-configuration.nix --flake .#weirdfish-cax11-4gb --target-host "root@weirdfi.sh"
 
-bootstrap-publicproxy:
+bootstrap-publicproxy: update-index
 	$(NIX) run github:nix-community/nixos-anywhere -- --generate-hardware-config nixos-generate-config ./systems/publicproxy-cax11-4gb/hardware-configuration.nix --flake .#publicproxy-cax11-4gb --target-host "root@publicproxy"
 
-deploy:
+deploy: update-index
 ifeq ($(HOSTNAME),hyperberry)
 	# build locally
 	deploy $(HOST) --skip-checks --skip-offline --fast-connection true -- --builders 'ssh://builder@miniberry aarch64-darwin - 16 1' --builders-use-substitutes --max-jobs 16 
--- presets/www/dcurgz.me/default.nix
+++ presets/www/dcurgz.me/default.nix
@@ -14,7 +14,41 @@ let
 
   inherit (pkgs.by.lib) replaceOptionalVars;
 
+  # mandoc escapes HTML in its '-T html' output mode. This reverses it.
+  unescapeHtml = ''
+    perl -pe 's{__RAW(.*?)__END}{
+      my $s=$1;
+      $s=~s/&amp;/&/g;
+      $s=~s/&gt;/>/g;
+      $s=~s/&lt;/</g;
+      $s=~s/&quot;/"/g;
+      $s }ge' \
+  '';
+  # __PREPROCESS(command)...__END blocks will invoke `command`, pass the block
+  # contents into stdin, and replace the whole block with the command output.
+  preprocessHtml = ''
+    perl -0777 -MIPC::Open2 -pe 's{__PREPROCESS\((.*?)\)(.*?)__END} {
+      my ($cmd, $block) = ($1, $2);
+      $block=~s/&amp;/&/g;
+      $block=~s/&gt;/>/g;
+      $block=~s/&lt;/</g;
+      $block=~s/&quot;/"/g;
+      my $pid = open2(my $out, my $in, $cmd);
+      print $in $block;
+      close $in;
+      local $/;
+      my $result = <$out>;
+      waitpid $pid, 0;
+      $result }gse' \
+  '';
+
   renderMdoc = path: lib.pipe path [
+    # 1. pre-processing
+    (path: replaceOptionalVars path {
+      # preset for rendering code-blocks with chroma
+      chroma = "chroma --html --html-only --html-prevent-surrounding-pre --html-lines";
+    })
+    # 2. render with mandoc and unescape
     (path: stdenv.mkDerivation (
       let
         name  = builtins.baseNameOf path;
@@ -22,15 +56,22 @@ let
       in
       {
         inherit name;
-        nativeBuildInputs = with pkgs; [ mandoc ];
+        nativeBuildInputs = with pkgs; [ mandoc perl chroma ];
         src = builtins.dirOf path;
+        # Render a .7 mdoc source file into HTML, unescape, pre-process, then
+        # write to output file.
         buildPhase = ''
-          mandoc -T html -O style=/style.css "$src/${name}" > ${name'}
+          mandoc -T html -O style=/style.css "$src/${name}" \
+            | ${unescapeHtml} \
+            | ${preprocessHtml} \
+            > ${name'} 
         '';
+        # Copy into the out directory.
         installPhase = ''
           cp ./${name'} "$out"
         '';
       }))
+    # 3. post-processing
     (path: replaceOptionalVars path {
       nix-gitrev =
         toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
@@ -48,6 +89,7 @@ let
   ];
 
   webroot = (pkgs.linkFarm "webroot" [
+    # TODO: Define this programmatically.
     {
       name = "index.html";
       path = renderMdoc ./index.7;
@@ -62,7 +104,16 @@ let
     }
     {
       name = "style.css";
-      path = ./style.css;
+      path = (stdenv.mkDerivation {
+        name = "compiled-styles";
+        src = ./.;
+        buildPhase = ''
+          cat *.css > ./output.css
+        '';
+        installPhase = ''
+          cp ./output.css $out
+        '';
+      });
     }
     {
       name = "rss.xml";
